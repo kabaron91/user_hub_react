60a0e9c2ea494455ed3875eae2c4cf9b55ff0151
Kate <kabaron91@gmail.com>
2021-05-24 12:46:35 -0500

    Building the Login/Logout Process

--- src/api/index.js
+++ src/api/index.js
@@ -4,27 +4,27 @@ const BASE = 'https://jsonplace-univclone.herokuapp.com'
 
 export async function getUsers() {
     try {
-        const { data } = await axios.get(`${ BASE }/users`);
-        return data;
+        const { data } = await axios.get(`${ BASE }/users`)
+        return data
     } catch (error) {
-        throw error;
+        throw error
     }
 }
 
 export async function getPostsByUser(userId) {
     try {
-        const { data } = await axios.get(`${ BASE }/users/${ userId }/posts`);
-        return data;
+        const { data } = await axios.get(`${ BASE }/users/${ userId }/posts`)
+        return data
     } catch (error) {
-        throw error;
+        throw error
     }
 }
 
 export async function getTodosByUser(userId) {
     try {
-        const { data } = await axios.get(`${ BASE }/users/${ userId }/todos`);
-        return data;
+        const { data } = await axios.get(`${ BASE }/users/${ userId }/todos`)
+        return data
     } catch (error) {
-        throw error;
+        throw error
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
+// src/index.js
+
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import { Header } from './components'
+import { getUsers } from './api'
 
 const App = () => {
-  return (
-    <div id="App">
-      <h1>Hello, World</h1>
-    </div>
-  );
+    const [userList, setUserList] = useState([])
+    useEffect(() => {
+        getUsers()
+        .then(users => {
+            setUserList(users)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    }, [])
+    return (
+        <div id="App">
+            <Header userList={ userList } />
+        </div>
+    )
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'))
